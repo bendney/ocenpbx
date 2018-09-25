a0daf7dbeebe1806b6a4aebed2f15cc8fd7b5430
root <root@localhost.localdomain>
2018-09-25 02:27:25 -0400

    Fix: progress of postgresql doesn't forward on

--- resources/postgresql.sh
+++ resources/postgresql.sh
@@ -24,7 +24,7 @@ verbose "Initalize PostgreSQL database"
 /usr/bin/postgresql-setup initdb
 
 #allow loopback
-sed -i '/listen_address/i listen_address = '*' ' /var/lib/pgsql/data/pg_hba.conf
+sed -i "/listen_addresses/i listen_addresses = '*' " /var/lib/pgsql/data/postgresql.conf
 sed -i 's/\(host  *all  *all  *0.0.0.0\/0  *\)ident/\1md5/' /var/lib/pgsql/data/pg_hba.conf
 sed -i 's/\(host  *all  *all  *::1\/128  *\)ident/\1md5/' /var/lib/pgsql/data/pg_hba.conf
 
@@ -37,8 +37,8 @@ cwd=$(pwd)
 cd /tmp
 
 #add the databases, users and grant permissions to them
-sudo -u postgres /usr/bin/psql -d ocenpbx -c "DROP SCHEMA public cascade;";
-sudo -u postgres /usr/bin/psql -d ocenpbx -c "CREATE SCHEMA public;";
+sudo -u postgres /usr/bin/psql -c "DROP SCHEMA public cascade;";
+sudo -u postgres /usr/bin/psql -c "CREATE SCHEMA public;";
 sudo -u postgres /usr/bin/psql -c "CREATE DATABASE ocenpbx";
 sudo -u postgres /usr/bin/psql -c "CREATE DATABASE freeswitch";
 sudo -u postgres /usr/bin/psql -c "CREATE ROLE ocenpbx WITH SUPERUSER LOGIN PASSWORD '$password';"
@@ -46,8 +46,8 @@ sudo -u postgres /usr/bin/psql -c "CREATE ROLE freeswitch WITH SUPERUSER LOGIN P
 sudo -u postgres /usr/bin/psql -c "GRANT ALL PRIVILEGES ON DATABASE ocenpbx to ocenpbx;"
 sudo -u postgres /usr/bin/psql -c "GRANT ALL PRIVILEGES ON DATABASE freeswitch to ocenpbx;"
 sudo -u postgres /usr/bin/psql -c "GRANT ALL PRIVILEGES ON DATABASE freeswitch to freeswitch;"
-sudo -u freeswitch /usr/bin/psql -d freeswitch -c "CREATE TABLE IF NOT EXISTS userinfo(username varchar(16) NOT NULL, password varchar(16) NOT NULL, dtmf varchar(16), qualify varchar(16), grade varchar(16));"
-sudo -u freeswitch /usr/bin/psql -d freeswitch -c "CREATE TABLE conference(confnum varchar(16) NOT NULL, confname varchar(16) NOT NULL, confuserpin varchar(16), confadminpin varchar(16), mode varchar(16));"
+sudo -u postgres /usr/bin/psql -d freeswitch -c "CREATE TABLE IF NOT EXISTS userinfo(username varchar(16) NOT NULL, password varchar(16) NOT NULL, dtmf varchar(16), qualify varchar(16), grade varchar(16));"
+sudo -u postgres /usr/bin/psql -d freeswitch -c "CREATE TABLE conference(confnum varchar(16) NOT NULL, confname varchar(16) NOT NULL, confuserpin varchar(16), confadminpin varchar(16), mode varchar(16));"
 #ALTER USER fusionpbx WITH PASSWORD 'newpassword';
 cd $cwd
 
